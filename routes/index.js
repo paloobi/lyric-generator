3cd2d601d4462491946d1e337656c65e3d4f13ca
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-01-30 15:00:39 -0500

    add /generate route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var router = require('express').Router();
 
-router.get('/', function(req, res){
+router.get('/', function(req, res, next){
 	res.render('index');
 });
 
 router.post('/generate', function(req, res, next) {
-  
+
 });
 
 module.exports = router;

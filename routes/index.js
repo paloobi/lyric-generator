db4f68a82643c384a4d3f55fcca5d9270a55b963
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-01-30 15:55:05 -0500

    generates random phrases

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,52 @@
 var router = require('express').Router();
+var words = require('../models/words');
 
-router.get('/', function(req, res, next){
+router.get('/', function(req, res){
 	res.render('index');
 });
 
 router.post('/generate', function(req, res, next) {
-  req.body.lines;
-  res.send();
+  var pattern = getRandomPattern();
+  console.log(pattern);
+  var phrase = pattern.map(function(type, idx){
+    var word = getWord(type);
+    if (idx === 0) word = word[0].toUpperCase() + word.slice(1);
+    return word;
+  });
+  console.log(phrase);
+  res.send(phrase.join(" ") + "\n");
 });
+
+function getWord(type) {
+  console.log('type is', type);
+  if (type === 'me' || type === 'you') {
+    return type;
+  }
+  var max = words[type].length;
+  var min = 0;
+  var idx = Math.floor(Math.random() * (max - min)) + min;
+  var word = words[type][idx];
+  if (type === "!") {
+    word += "!";
+  }
+  return word;
+}
+
+var phrasePatterns = [
+  ["D","A", "N", "t", "me"],
+  ["D", "A", "h", "t", 'you', "A" ],
+  ["D", "A", "N", "i", "you"],
+  ["t", "me"],
+  ["i", "you"],
+  ["you", "t", "D", "h"],
+  ["ex", "ex"]
+]
+
+function getRandomPattern() {
+  var max = phrasePatterns.length;
+  var min = 0;
+  var idx = Math.floor(Math.random() * (max - min)) + min;
+  return phrasePatterns[idx];
+}
 
 module.exports = router;

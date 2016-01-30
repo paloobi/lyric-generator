fb7105c685ebe9d79a80ba93dfb0891bec63fd61
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-01-30 16:07:37 -0500

    modularize generator logic into models/generator

---
+++ models/generator.js
@@ -0,0 +1,55 @@
+var words = require('./words');
+
+function getWord(type) {
+  console.log('type is', type);
+  if (type === 'me' || type === 'you') {
+    return type;
+  }
+  var max = words[type].length;
+  var min = 0;
+  var idx = Math.floor(Math.random() * (max - min)) + min;
+  var word = words[type][idx];
+  if (type === "!") {
+    word += "!";
+  }
+  return word;
+}
+
+var phrasePatterns = [
+  ["D","A", "N", "t", "me"],
+  ["D", "A", "h", "t", 'obj', "A" ],
+  ["D", "A", "N", "i", "obj"],
+  ["t", "me"],
+  ["i", "you"],
+  ["you", "t", "D", "h"],
+  ["ex", "ex"]
+]
+
+function getRandomPattern() {
+  var max = phrasePatterns.length;
+  var min = 0;
+  var idx = Math.floor(Math.random() * (max - min)) + min;
+  return phrasePatterns[idx];
+}
+
+function getLine() {
+  var pattern = getRandomPattern();
+
+  var phrase = pattern.map(function(type, idx){
+    var word = getWord(type);
+    if (idx === 0) word = word[0].toUpperCase() + word.slice(1);
+    return word;
+  });
+
+  return phrase.join(" ") + "\n";
+}
+
+function getPoem(length) {
+  var poem = "";
+  for (var i = 0 ; i < length; i++) {
+    poem += getLine();
+  }
+  return poem;
+}
+
+module.exports.getPoem = getPoem;
--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,14 @@
 var router = require('express').Router();
-var words = require('../models/words');
+var getPoem = require('../models/generator').getPoem;
 
 router.get('/', function(req, res){
 	res.render('index');
 });
 
 router.post('/generate', function(req, res, next) {
-  var pattern = getRandomPattern();
-  console.log(pattern);
-  var phrase = pattern.map(function(type, idx){
-    var word = getWord(type);
-    if (idx === 0) word = word[0].toUpperCase() + word.slice(1);
-    return word;
-  });
-  console.log(phrase);
-  res.send(phrase.join(" ") + "\n");
+  var poem = getPoem(3);
+  console.log(poem);
+  res.send(poem);
 });
-
-function getWord(type) {
-  console.log('type is', type);
-  if (type === 'me' || type === 'you') {
-    return type;
-  }
-  var max = words[type].length;
-  var min = 0;
-  var idx = Math.floor(Math.random() * (max - min)) + min;
-  var word = words[type][idx];
-  if (type === "!") {
-    word += "!";
-  }
-  return word;
-}
-
-var phrasePatterns = [
-  ["D","A", "N", "t", "me"],
-  ["D", "A", "h", "t", 'you', "A" ],
-  ["D", "A", "N", "i", "you"],
-  ["t", "me"],
-  ["i", "you"],
-  ["you", "t", "D", "h"],
-  ["ex", "ex"]
-]
-
-function getRandomPattern() {
-  var max = phrasePatterns.length;
-  var min = 0;
-  var idx = Math.floor(Math.random() * (max - min)) + min;
-  return phrasePatterns[idx];
-}
 
 module.exports = router;
